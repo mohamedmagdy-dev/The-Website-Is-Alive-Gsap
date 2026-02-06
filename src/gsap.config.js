a1571e3ec828905d4671e0af37ce29fc4408f12f
Mohamed Magdy Elsayed <mohamedmagdyelsayed7@gmail.com>
2026-02-06 19:57:40 +0200

    cleanup gsap config

--- src/gsap.config.js
+++ src/gsap.config.js
@@ -1,17 +1,10 @@
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 
-import { Draggable } from "gsap/Draggable";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { Flip } from "gsap/Flip";
 import { GSDevTools } from "gsap/GSDevTools";
-import { MotionPathHelper } from "gsap/MotionPathHelper";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { Observer } from "gsap/Observer";
 import { Physics2DPlugin } from "gsap/Physics2DPlugin";
-import { PhysicsPropsPlugin } from "gsap/PhysicsPropsPlugin";
-import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
@@ -20,17 +13,11 @@ import { TextPlugin } from "gsap/TextPlugin";
 
 gsap.registerPlugin(
   useGSAP,
-  Draggable,
-  DrawSVGPlugin,
-  Flip,
+
   GSDevTools,
-  MotionPathHelper,
-  MotionPathPlugin,
   MorphSVGPlugin,
   Observer,
   Physics2DPlugin,
-  PhysicsPropsPlugin,
-  ScrambleTextPlugin,
   ScrollTrigger,
   ScrollSmoother,
   ScrollToPlugin,
